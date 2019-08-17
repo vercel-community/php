bbb1eaa3f3f0f100775bd273091968e1b7071269
Milan Felix Šulc <rkfelix@gmail.com>
2019-08-17 21:28:18 +0200

    QA: indent ts as js

--- packages/php/src/launchers/helpers.ts
+++ packages/php/src/launchers/helpers.ts
@@ -6,62 +6,62 @@ export const getPhpDir = (): string => pathJoin(process.env.LAMBDA_TASK_ROOT || 
 export const isDev = (): boolean => process.env.NOW_PHP_DEV === '1';
 
 export function normalizeEvent(event: Event): AwsRequest {
-    if (event.Action === 'Invoke') {
-        const invokeEvent = JSON.parse(event.body);
-
-        const {
-            method, path, host, headers = {}, encoding,
-        }: InvokedEvent = invokeEvent;
-
-        let { body } = invokeEvent;
-
-        if (body) {
-            if (encoding === 'base64') {
-                body = Buffer.from(body, encoding);
-            } else if (encoding === undefined) {
-                body = Buffer.from(body);
-            } else {
-                throw new Error(`Unsupported encoding: ${encoding}`);
-            }
-        }
-
-        return {
-            method,
-            path,
-            host,
-            headers,
-            body,
-        };
-    }
+  if (event.Action === 'Invoke') {
+    const invokeEvent = JSON.parse(event.body);
 
     const {
-        httpMethod: method, path, host, headers = {}, body,
-    } = event;
+      method, path, host, headers = {}, encoding,
+    }: InvokedEvent = invokeEvent;
+
+    let { body } = invokeEvent;
+
+    if (body) {
+      if (encoding === 'base64') {
+        body = Buffer.from(body, encoding);
+      } else if (encoding === undefined) {
+        body = Buffer.from(body);
+      } else {
+        throw new Error(`Unsupported encoding: ${encoding}`);
+      }
+    }
 
     return {
-        method,
-        path,
-        host,
-        headers,
-        body,
+      method,
+      path,
+      host,
+      headers,
+      body,
     };
+  }
+
+  const {
+    httpMethod: method, path, host, headers = {}, body,
+  } = event;
+
+  return {
+    method,
+    path,
+    host,
+    headers,
+    body,
+  };
 }
 
 export async function transformFromAwsRequest({
-    method, path, host, headers, body,
+  method, path, host, headers, body,
 }: AwsRequest): Promise<PhpInput> {
-    const { pathname, search } = urlParse(path);
+  const { pathname, search } = urlParse(path);
 
-    const filename = pathJoin(
-        getUserDir(),
-        process.env.NOW_ENTRYPOINT || pathname || '',
-    );
+  const filename = pathJoin(
+    getUserDir(),
+    process.env.NOW_ENTRYPOINT || pathname || '',
+  );
 
-    const uri = pathname + (search || '');
+  const uri = pathname + (search || '');
 
-    return { filename, path, uri, host, method, headers, body };
+  return { filename, path, uri, host, method, headers, body };
 }
 
 export function transformToAwsResponse({ statusCode, headers, body }: PhpOutput): AwsResponse {
-    return { statusCode, headers, body: body.toString('base64'), encoding: 'base64' };
+  return { statusCode, headers, body: body.toString('base64'), encoding: 'base64' };
 }
--- packages/php/src/types.d.ts
+++ packages/php/src/types.d.ts
@@ -1,82 +1,82 @@
 type Headers = { [k: string]: string | string[] | undefined };
 
 interface Files {
-    [filePath: string]: import('@now/build-utils').File;
+  [filePath: string]: import('@now/build-utils').File;
 }
 
 interface MetaOptions {
-    meta: import('@now/build-utils').Meta;
+  meta: import('@now/build-utils').Meta;
 }
 
 interface AwsRequest {
-    method: string,
-    path: string,
-    host: string,
-    headers: Headers,
-    body: string,
+  method: string,
+  path: string,
+  host: string,
+  headers: Headers,
+  body: string,
 }
 
 interface AwsResponse {
-    statusCode: number,
-    headers: Headers,
-    body: string,
-    encoding?: string
+  statusCode: number,
+  headers: Headers,
+  body: string,
+  encoding?: string
 }
 
 interface Event {
-    Action: string,
-    body: string,
-    httpMethod: string,
-    path: string,
-    host: string,
-    headers: Headers,
-    encoding: string | undefined | null,
+  Action: string,
+  body: string,
+  httpMethod: string,
+  path: string,
+  host: string,
+  headers: Headers,
+  encoding: string | undefined | null,
 }
 
 interface InvokedEvent {
-    method: string,
-    path: string,
-    host: string,
-    headers: Headers,
-    encoding: string | undefined | null,
+  method: string,
+  path: string,
+  host: string,
+  headers: Headers,
+  encoding: string | undefined | null,
 }
 
 interface CgiInput {
-    filename: string,
-    path: string,
-    host: string,
-    method: string,
-    headers: Headers,
+  filename: string,
+  path: string,
+  host: string,
+  method: string,
+  headers: Headers,
 }
 
 interface PhpInput {
-    filename: string,
-    path: string,
-    uri: string,
-    host: string,
-    method: string,
-    headers: Headers,
-    body: string,
+  filename: string,
+  path: string,
+  uri: string,
+  host: string,
+  method: string,
+  headers: Headers,
+  body: string,
 }
 
 interface PhpOutput {
-    statusCode: number,
-    headers: Headers,
-    body: Buffer,
+  statusCode: number,
+  headers: Headers,
+  body: Buffer,
 }
 
 interface CgiHeaders {
-    [k: string]: string,
+  [k: string]: string,
 }
 
 interface CgiRequest {
-    env: Env,
+  env: Env,
 }
 
 interface Env {
-    [k: string]: any,
+  [k: string]: any,
 }
 
 interface PhpIni {
-    [k: string]: any,
+  [k: string]: any,
 }
