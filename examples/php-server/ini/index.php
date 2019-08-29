de84a3d265004a80983b911679390d7172404727
Milan Felix Šulc <rkfelix@gmail.com>
2019-08-29 09:23:10 +0200

    Examples: better php ini view

--- examples/php-server/ini/index.php
+++ examples/php-server/ini/index.php
@@ -1,8 +1,24 @@
-<?php
+<h1>php.ini</h1>
 
+<table>
+<thead>
+    <tr>
+        <th>option</th>
+        <th>global_value</th>
+        <th>local_value</th>
+        <th>access</th>
+    </tr>
+</thead>
+<tbody>
+<?php
 foreach (ini_get_all() as $name => $group) {
-    echo "<h1>$name</h1>";
-    foreach ($group as $k => $v) {
-        echo "$k => $v<br>";
-    }
+    echo "<tr>";
+    echo sprintf('<td>%s</td>', $name);
+    echo sprintf('<td>%s</td>', $group['global_value']);
+    echo sprintf('<td>%s</td>', $group['local_value']);
+    echo sprintf('<td>%s</td>', $group['access']);
+    echo "</tr>";
 }
+?>
+</tbody>
+</table>
