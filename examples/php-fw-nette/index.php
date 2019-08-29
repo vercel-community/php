4e8d204493fd9b6d5cf5e118912d9504a37421df
Milan Felix Šulc <rkfelix@gmail.com>
2019-08-29 10:20:55 +0200

    Juicy sync

--- examples/php-fw-nette/index.php
+++ examples/php-fw-nette/index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>JuicyFx PHP Now Builders</title>
+    <title>Juicy(fx) PHP Now Builders</title>
 </head>
 <body>
-    <h1>JuicyFx PHP Now Builders</h1>
+    <h1>Juicy(fx) PHP Now Builders</h1>
 
     <ul>
         <li><a href="/phpinfo.php">PHPINFO</a></li>
