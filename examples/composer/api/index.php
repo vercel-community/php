2a8a1aa7a964ca9b4ca7b7ceb7d3249c32428b59
Milan Felix Šulc <rkfelix@gmail.com>
2020-01-16 10:36:01 +0100

    Refactoring: move examples to solo repository

--- examples/composer/api/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-Tracy\Debugger::enable(Tracy\Debugger::DEVELOPMENT);
-
-throw new RuntimeException('Hello Tracy!');
--- examples/framework-nette/assets/nginx/checker.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Content-Type: application/javascript');
-?>
-nginxChecker = <?php echo isset($_GET['ok']) ? 1 : 0; ?>;
