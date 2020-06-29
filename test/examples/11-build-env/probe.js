a41fe49f19b4c7884f490fe149e4c8d81753e09a
Milan Felix Šulc <rkfelix@gmail.com>
2020-06-29 22:08:41 +0200

    Tests: drop probes

--- test/examples/11-build-env/probe.js
+++
@@ -1,6 +0,0 @@
-const assert = require('assert');
-
-module.exports = async ({ deploymentUrl, fetch }) => {
-  const resp = await fetch(`https://${deploymentUrl}/index.php`);
-  assert.deepStrictEqual(await resp.text(), 'bool(true)\n');
-};
